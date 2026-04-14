fc116c25ea9af5cd8487ada169d907ffafa36f82
Happy <88503684+Happyisdeveloper@users.noreply.github.com>
2026-04-14 20:15:04 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,201 @@
+window.__ARCHIVE_DATA__ = {
+  "allTags": [
+    {
+      "name": "Favori",
+      "dot": "#EF9F27",
+      "bg": "#FAEEDA",
+      "color": "#854F0B"
+    },
+    {
+      "name": "Happy",
+      "dot": "#e6761e",
+      "bg": "#eac6a9",
+      "color": "#f4d8c2"
+    },
+    {
+      "name": "Turbo",
+      "dot": "#d8bb18",
+      "bg": "#e6da99",
+      "color": "#f1e8b1"
+    },
+    {
+      "name": "Sketch",
+      "dot": "#e8305a",
+      "bg": "#eebac6",
+      "color": "#f7d4dc"
+    },
+    {
+      "name": "Panik",
+      "dot": "#b3141b",
+      "bg": "#dd787d",
+      "color": "#eb8e93"
+    },
+    {
+      "name": "Altwo",
+      "dot": "#3e9dea",
+      "bg": "#c6def1",
+      "color": "#0e314e"
+    },
+    {
+      "name": "Pg Crew",
+      "dot": "#e6192c",
+      "bg": "#e9a5ab",
+      "color": "#f4bec3"
+    },
+    {
+      "name": "1st Crew",
+      "dot": "#35e9ae",
+      "bg": "#beefdf",
+      "color": "#0c4533"
+    },
+    {
+      "name": "Reas",
+      "dot": "#19e641",
+      "bg": "#a5e9b2",
+      "color": "#bef4c8"
+    },
+    {
+      "name": "Opie",
+      "dot": "#5418d8",
+      "bg": "#b199e6",
+      "color": "#c5b1f1"
+    },
+    {
+      "name": "Rose",
+      "dot": "#edb25a",
+      "bg": "#f7edde",
+      "color": "#684612"
+    },
+    {
+      "name": "Hof",
+      "dot": "#c3def8",
+      "bg": "#deebf7",
+      "color": "#2478cc"
+    },
+    {
+      "name": "Tag",
+      "dot": "#1980e6",
+      "bg": "#a5c7e9",
+      "color": "#bed9f4"
+    },
+    {
+      "name": "Karakter",
+      "dot": "#934d10",
+      "bg": "#d6955c",
+      "color": "#e6a770"
+    },
+    {
+      "name": "Hakko",
+      "dot": "#d69af4",
+      "bg": "#efdef7",
+      "color": "#781da5"
+    },
+    {
+      "name": "Rode",
+      "dot": "#ebc94c",
+      "bg": "#f4edd2",
+      "color": "#5b4b10"
+    },
+    {
+      "name": "Cenk",
+      "dot": "#2c1093",
+      "bg": "#765cd6",
+      "color": "#8970e6"
+    },
+    {
+      "name": "Paps",
+      "dot": "#e075f0",
+      "bg": "#f4def7",
+      "color": "#741782"
+    },
+    {
+      "name": "Kan Crew",
+      "dot": "#770d15",
+      "bg": "#d0434e",
+      "color": "#e15660"
+    },
+    {
+      "name": "Tur Crew",
+      "dot": "#e72326",
+      "bg": "#ebadae",
+      "color": "#f5c7c7"
+    },
+    {
+      "name": "Ostik",
+      "dot": "#0f8a3a",
+      "bg": "#54d481",
+      "color": "#67e493"
+    },
+    {
+      "name": "Bueno",
+      "dot": "#a15012",
+      "bg": "#d99a68",
+      "color": "#e8ac7d"
+    },
+    {
+      "name": "Semiok",
+      "dot": "#9a47eb",
+      "bg": "#e1cef3",
+      "color": "#340f57"
+    },
+    {
+      "name": "Hey Morik",
+      "dot": "#a455ec",
+      "bg": "#e9daf6",
+      "color": "#3c1264"
+    },
+    {
+      "name": "Etkinlik",
+      "dot": "#3c070e",
+      "bg": "#a72a3b",
+      "color": "#d0253c"
+    },
+    {
+      "name": "Kae",
+      "dot": "#730d77",
+      "bg": "#cb43d0",
+      "color": "#dc56e1"
+    }
+  ],
+  "folderTree": [
+    {
+      "id": 1,
+      "name": "Black Book'um",
+      "children": [],
+      "collapsed": false
+    },
+    {
+      "id": 2,
+      "name": "Duvarlarım",
+      "children": [],
+      "collapsed": false
+    },
+    {
+      "id": 3,
+      "name": "Duvarlar",
+      "children": [],
+      "collapsed": false
+    },
+    {
+      "id": 4,
+      "name": "2026 Bostancı Hof",
+      "children": [],
+      "collapsed": false
+    },
+    {
+      "id": 5,
+      "name": "Etkinlik",
+      "children": [
+        {
+          "id": 6,
+          "name": "Hof",
+          "children": [],
+          "collapsed": false
+        }
+      ],
+      "collapsed": false
+    }
+  ],
+  "files": [],
+  "exportedAt": "2026-04-14T17:13:23.513Z"
+};
